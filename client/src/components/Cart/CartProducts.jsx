a57a107c18903bd376c02f93ac6f765f16276661
hanisx019 <anisx019@gmail.com>
2025-09-25 20:25:46 +0530

    Add CartDrawer

---
+++ client/src/components/Cart/CartProducts.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const CartProducts = () => {
+
+    const products=[
+        {
+            id:1,
+            name:"T-shirt",
+            size:"M",
+            color:"Black",
+            price:20,
+            image:"https://picsum.photos/200?random=1",
+        },
+        {
+            id:2,
+            name:"Pant",
+            size:"L",
+            color:"yellow",
+            price:3000,
+            image:"https://picsum.photos/200?random=2",
+        },
+        {
+            id:3,
+            name:"Skirt",
+            size:"M",
+            color:"Blue",
+            price:340,
+            image:"https://picsum.photos/200?random=3",
+        },
+        {
+            id:4,
+            name:"T-shirt",
+            size:"XXL",
+            color:"Green",
+            price:120,
+            image:"https://picsum.photos/200?random=4",
+        },
+        {
+            id:5,
+            name:"Jeans",
+            size:"XL",
+            color:"Violet",
+            price:270,
+            image:"https://picsum.photos/200?random=5",
+        },
+        {
+            id:6,
+            name:"shervani",
+            size:"S",
+            color:"Purple",
+            price:20,
+            image:"https://picsum.photos/200?random=6",
+        },
+        {
+            id:7,
+            name:"Tank Tops",
+            size:"M",
+            color:"Black",
+            price:209,
+            image:"https://picsum.photos/200?random=7",
+        },
+        {
+            id:8,
+            name:"Jackets",
+            size:"L",
+            color:"White",
+            price:20,
+            image:"https://picsum.photos/200?random=8",
+        },
+    ]
+
+  return (
+    <div className=''>
+        {products.map((products,index)=>(
+        <div key={index} className='flex gap-4 py-2 w-full border-b-1 border-gray-400'>
+          <img className='h-20 w-[25%] md:h-22 md:w-[22%] bg-black text-[20px] ml-5 object-cover' src={products.image} />
+          <div className='flex w-3/4 justify-between items-center gap-4'>
+            <div className='w-[70%]'>
+              <div >
+                <div className='text-[14px] md:text-[16px] font-semibold '>
+                  <h2>{products.name}</h2>
+                </div>
+                <div className='flex gap-3 text-gray-600 text-[10px] md:text-[14px] items-center'>
+                  <span className="flex items-center">
+                    <span className="w-[60px]">Size :<span> {products.size}</span></span>
+                    <span className="h-4 border-r-2   -ml-4  -mr-1  md:-ml-1 md:-mr-1  border-gray-400 "></span>
+                  </span>
+                  <span className='text-nowrap'>Color : <span>{products.color}</span></span>
+                </div>
+              </div>
+              <div className='flex items-center gap-2 mt-2'>
+                <button className=' border-2 border-gray-300 py-1 px-2 rounded-[6px]'>-</button>
+                <p className='font-bold'>1</p>
+                <button className=' border-2 border-gray-300 py-1 px-2 rounded-[6px]'>+</button>
+              </div>
+            </div>
+            <div className='ml-[-50px] w-[80px] flex flex-col items-center justify-center min-h-[60px]'>
+              <h2 className='font-bold -mt-[4px] md:mt-0 text-[15px] md:text-[18px] my-1'>${products.price}</h2>
+              <br></br>
+              <p className='text-red-900 text-[12px] bg-opacity-10 rounded px-2 py-1 w-full text-center cursor-pointer'>Remove</p>
+            </div>
+        </div>
+      </div>   
+        ))}
+    </div>
+  )
+}
+
+export default CartProducts
--- client/src/components/Common/Header/Cartbar/Cartbar.jsx
+++ client/src/components/Common/Header/Cartbar/Cartbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { IoMdClose } from "react-icons/io";
+import CartProducts from '../../../Cart/CartProducts';
+
 
 const Cartbar = ({openCartbar,setOpenCartBar}) => {
       const toggleCartBar=()=>{
@@ -8,8 +10,17 @@ const Cartbar = ({openCartbar,setOpenCartBar}) => {
   return (
     <div className={`fixed right-0 top-0 w-2/3 md:w-2/4 lg:w-1/3 2xl:w-1/4 bg-white  shadow-2xl h-full  transition-all duration-300  ${openCartbar?'translate-x-0 ':'translate-x-full'}`} >
       <div>
-        <h3 className='text-left py-13 md:py-16 px-3 font-bold text-[20px] select-none'>Your Cart </h3>
+        <h3 className='text-left py-13 md:py-16 px-3 font-medium text-[22px] select-none'>Cart Overview</h3>
         <IoMdClose onClick={toggleCartBar} className='size-8 mr-3 mt-3 absolute top-0 right-0 cursor-pointer'/>
+      </div>
+
+  <div className='mt-[-39px] md:mt-[-50px] h-[72vh] overflow-y-scroll cartbar-scroll-area border-t-2 border-gray-200 '>
+  <CartProducts/>
+  </div>
+
+      <div className='flex flex-col items-center h-[14vh] border-t-2 border-gray-200 pt-2  absolute bottom-4 w-full bg-white'>
+        <button className='w-[95%] bg-black text-white mt-4 ml-1 p-2 rounded-[7px] hover:bg-black/85 transition-all  cursor-pointer'>Checkout</button>
+        <p className='text-center text-[13px] mt-2 font-light '>Shipping, Taxes, and Discount Codes Calculated at Checkout</p>
       </div>
     </div>
   )
