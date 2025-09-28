4613eed6f388d5289da40482f7d8a81cf0cd62b1
hanisx019 <anisx019@gmail.com>
2025-09-28 21:33:09 +0530

    Fixe Womens Scetion Bugs

--- client/src/components/Main/Home/Apparel.jsx
+++ client/src/components/Main/Home/Apparel.jsx
@@ -6,31 +6,31 @@ import { CiCreditCard1 } from "react-icons/ci";
 const Apparel = () => {
   return (
     <div>
-        <div className='flex py-4 px-8 mt-[200px] h-[90vh] '>
-            <div className='w-1/2 flex flex-col justify-center items-start gap-4 px-12 rounded-l-4xl bg-black text-white'>
-                <h2 className='text-lg'>Comfort and Style</h2>
-                <h3 className='text-4xl font-bold'>Apparel Made For Your<br/> Everyday Life</h3>
-                <p className='text-sm'>Dicover high qualtiy, comfortable clothing that effortlessly blends fashion and<br/> function. Designed to make you look and feel great everyday</p>
-                <button className='text-black bg-white py-2 px-6 font-semibold rounded-md'>Shop Now</button>
+        <div className='flex-col md:flex md:flex-row py-4 px-8 mt-[200px] h-[90vh] '>
+            <div className='w-full md:w-1/2 flex flex-col justify-center items-start gap-4 px-12 rounded-t-4xl md:rounded-l-4xl md:rounded-t-none bg-black text-white'>
+                <h2 className='text-lg mt-5 md:mt-0'>Comfort and Style</h2>
+                <h3 className='text-3xl md:text-4xl font-bold -mt-3 md:mt-0'>Apparel Made For Your<br className='hidden md:block'/> Everyday Life</h3>
+                <p className='text-sm -mt-2 md:mt-0'>Dicover high qualtiy, comfortable clothing that effortlessly blends fashion and<br/> function. Designed to make you look and feel great everyday</p>
+                <button className='text-black bg-white py-2 px-6 font-semibold rounded-md -mt-1 md:mt-0 mb-2 md:mb-0'>Shop Now</button>
             </div>
-            <img className='w-1/2 object-cover rounded-r-4xl' src="https://res.cloudinary.com/dfwq8rih1/image/upload/v1759068669/g2_jfz83f.jpg"/>
+            <img className='w-full md:w-1/2 object-cover rounded-b-4xl md:rounded-r-4xl md:rounded-b-none md:rounded-br-4xl' src="https://res.cloudinary.com/dfwq8rih1/image/upload/v1759068669/g2_jfz83f.jpg"/>
         </div>
 
-        <div className='mt-30 flex justify-around items-center mb-16'>
+        <div className='-mt-50 md:mt-30 flex-col md:flex md:flex-row justify-around items-center mb-16'>
             <div className='flex flex-col items-center'>
                 <HiShoppingBag className='size-5' />
                 <h3 className='mt-6 font-semibold'>FREE INTERNATIONAL SHIPPING</h3>
-                <h4 className='mt-1 text-sm font-semibold text-neutral-600'>On all order's over $100.0</h4>
+                <h4 className='mb-10 md:mb-0 mt-1 text-sm font-semibold text-neutral-600'>On all order's over $100.0</h4>
             </div>
             <div className='flex flex-col items-center'>
                 <MdReplay className='size-5'/>
                 <h3 className='mt-6 font-semibold'>45 DAYS RETURNS</h3>
-                <h4 className='mt-1 text-sm font-semibold text-neutral-600'>Money back gurantee</h4>
+                <h4 className='mb-10 md:mb-0 mt-1 text-sm font-semibold text-neutral-600'>Money back gurantee</h4>
             </div>
             <div className='flex flex-col items-center'>
                 <CiCreditCard1 className='size-5' />
                 <h3 className='mt-6 font-semibold'>SECURE CHECKOUT</h3>
-                <h4 className='mt-1 text-sm font-semibold text-neutral-600'>100% secure payment</h4>
+                <h4 className='mb-10 md:mb-0 mt-1 text-sm font-semibold text-neutral-600'>100% secure payment</h4>
             </div>
         </div>
     </div>
--- client/src/components/Main/Home/WomensSection.jsx
+++ client/src/components/Main/Home/WomensSection.jsx
@@ -16,7 +16,7 @@ const WomensSection = () => {
             name:"Wide-Leg Trousers",
             price:120,
             image:{
-                url:"https://res.cloudinary.com/dfwq8rih1/image/upload/v1759067484/wm4_vrgwjf.jpg",
+                url:"https://res.cloudinary.com/dfwq8rih1/image/upload/v1759067359/wm8_b9phy9.jpg",
             },
         },
         {
@@ -40,7 +40,7 @@ const WomensSection = () => {
             name:"Wide-Leg Trousers",
             price:120,
             image:{
-                url:"https://res.cloudinary.com/dfwq8rih1/image/upload/v1759067359/wm8_b9phy9.jpg",
+                url:"https://res.cloudinary.com/dfwq8rih1/image/upload/v1759067484/wm4_vrgwjf.jpg",
             },
         },
         {
@@ -74,18 +74,29 @@ const WomensSection = () => {
         <h2 className="py-2 text-center text-5xl uppercase font-bold">Top Wear For Womens</h2>
         <h3 className="text-center text-2xl uppercase font-bold">Chic Fits for Every Mood.</h3>
         <div className='flex flex-col gap-6 w-full'>
-            <div className='flex flex-row gap-12 px-12 w-full mt-10'>
-                {womens.slice(0, 4).map((item) => (
-                    <div className='w-1/4 ' key={item._id}>
+            {/* Mobile: show only first two images */}
+            <div className='flex flex-col lg:flex-row gap-12 px-12 w-full mt-10 md:hidden'>
+                {womens.slice(0, 2).map((item) => (
+                    <div className='w-full flex-wrap' key={item._id}>
                         <img className='w-full h-100 object-cover rounded-3xl' src={item.image.url} loading="eager" width="300" height="400"/>
                         <h3 className='px-3 pt-2 font-semibold'>{item.name}</h3>
                         <h4 className='px-3 pt-1 font-semibold text-neutral-600'>${item.price}</h4>
                     </div>
                 ))}
             </div>
-            <div className='flex flex-row gap-12 px-12 w-full'>
+            {/* Desktop/Tablet: show all images as before */}
+            <div className='hidden md:flex flex-col lg:flex-row gap-12 px-12 w-full mt-10'>
+                {womens.slice(0, 4).map((item) => (
+                    <div className='w-full lg:w-1/4' key={item._id}>
+                        <img className='w-full h-100 object-cover rounded-3xl' src={item.image.url} loading="eager" width="300" height="400"/>
+                        <h3 className='px-3 pt-2 font-semibold'>{item.name}</h3>
+                        <h4 className='px-3 pt-1 font-semibold text-neutral-600'>${item.price}</h4>
+                    </div>
+                ))}
+            </div>
+            <div className='hidden md:flex flex-col lg:flex-row gap-12 px-12 w-full'>
                 {womens.slice(4, 8).map((item) => (
-                    <div className='w-1/4' key={item._id}>
+                    <div className='w-full lg:w-1/4' key={item._id}>
                         <img className='w-full h-100 object-cover rounded-3xl' src={item.image.url} loading="eager" width="300" height="400"/>
                         <h3 className='px-3 pt-2 font-semibold'>{item.name}</h3>
                         <h4 className='px-3 pt-1 font-semibold text-neutral-600'>${item.price}</h4>
